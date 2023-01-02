701189ac8a6106e805761291c298651c130d3ba9
Roger Davila <rogerddavila@gmail.com>
2023-01-01 18:29:58 -0800

    Initial Commit

---
+++ Near/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Near
+//
+//  Created by Roger Davila on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
